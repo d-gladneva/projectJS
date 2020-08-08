e0902450b01b86a2e6f3fde19901c705c1c50483
Dasha <davydova_di@mail.ru>
2020-08-08 16:34:17 +0300

    feat: assignment of values|script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const money = 1;
-const income = 2;
-const addExpenses = 3;
-const deposit = 4;
-const mission = 5;
+const money = 100;
+const income = 'фриланс';
+const addExpenses = 'интернет, такси, коммуналка';
+const deposit = true;
+const mission = 50000;
 const period = 6;
+const budgetDay = 30000;
 
-alert('Hello!');
+console.log(money, income, deposit);
+console.log(addExpenses.length);
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей');
+console.log(budgetDay);
 
-console.log('Hello!');
+const addExpensesLower = addExpenses.toLowerCase();
+const space = ' ';
+console.log(addExpensesLower.split(space));
