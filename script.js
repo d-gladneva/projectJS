b873d43de4d537f0b45a564dcc99f57f9af2bed1
Dasha <davydova_di@mail.ru>
2020-08-09 12:31:31 +0300

    fix: variable declaration|script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const money = 1;
-const income = 2;
-const addExpenses = 3;
-const deposit = 4;
-const mission = 5;
-const period = 6;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
 alert('Hello!');
 
