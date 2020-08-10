2cbbd901957a3a603ce3269973f3d3b411855dc7
Dasha <davydova_di@mail.ru>
2020-08-10 10:58:23 +0300

    fix: correct console output

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const money = 100;
 const income = 'фриланс';
-const addExpenses = 'интернет, такси, коммуналка';
+const addExpenses = 'Интернет, Такси, Коммуналка';
 const deposit = true;
 const mission = 50000;
 const period = 6;
-const budgetDay = 30000;
+const budgetDay = money/30;
 
 console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен', period, 'месяцев');
 console.log('Цель заработать', mission, 'рублей');
+console.log(addExpenses.toLowerCase().split(' '));
 console.log(budgetDay);
-
-console.log(addExpenses.toLowerCase().split(''));
