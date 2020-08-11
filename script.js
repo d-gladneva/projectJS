3dbcab82d6216b0532c2b3555702bcef9ca75899
Dasha <davydova_di@mail.ru>
2020-08-11 18:33:58 +0300

    feat: create functions | script.js

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 const money = prompt('Ваш месячный доход?');
-console.log(money);
 const income = 'фриланс';
 const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(typeof addExpenses);
 const deposit = confirm('Есть ли у Вас депозит в банке?');
-console.log(deposit);
 const mission = 120000;
 const period = 6;
 
-console.log(typeof money, typeof income, typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', mission, 'рублей');
-console.log(addExpenses.toLowerCase().split(' '));
+let showTypeOf = function(data){
+    console.log(data, typeof(data));
+};
 
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
-const expenses1 = String(prompt('Введите обязательную статью расходов?'));
+const expenses1 = prompt('Введите обязательную статью расходов?');
 const amount1 = prompt('Во сколько это обойдется?');
 
 const expenses2 = prompt('Введите обязательную статью расходов?');
 const amount2 = prompt('Во сколько это обойдется?');
 
-const budgetMonth = +money - (+amount1 + +amount2);
-const budgetDay = parseInt( budgetMonth / 30);
-console.log('Бюджет на месяц:', budgetMonth);
-const missionMonths = Math.round(mission / budgetMonth);
+const getExpensesMonth = function (exp1, exp2) {
+    return console.log(+exp1 + +exp2);
+};
+
+console.log(addExpenses.toLowerCase().split(' '));
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей');
+
+
+const getAccumulatedMonth = function (inc) {
+    return inc - getExpensesMonth(amount1, amount2);
+};
+
+const accumulatedMonth = getAccumulatedMonth(money);
+
+const getTargetMonth = function(month){
+    return month/30;
+};
+
+const budgetDay = parseInt(getTargetMonth(accumulatedMonth) / 30);
+const missionMonths = Math.round(mission / getTargetMonth(accumulatedMonth));
 console.log('Цель будет достигнута за:', missionMonths);
 console.log('Бюджет за день:', budgetDay);
 
 
-if (budgetDay >= 1200) {
-    console.log('У Вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay < 1200) {
-    console.log('У Вас средний уровень дохода');
-} else if (budgetDay < 600) {
-    console.log('К сожалению, у Вас уровень дохода ниже среднего');
-}
+let getStatusIncome = function(){
+    if (budgetDay >= 1200) {
+        console.log('У Вас высокий уровень дохода');
+    } else if (budgetDay >= 600 && budgetDay < 1200) {
+        console.log('У Вас средний уровень дохода');
+    } else if (budgetDay < 600) {
+        console.log('К сожалению, у Вас уровень дохода ниже среднего');
+    }
+};
+
+getStatusIncome();
 
 
 
